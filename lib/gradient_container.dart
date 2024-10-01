75a570319ca2ddf728e77ea157d34d365849e273
JDiMagg <jdimag@nyit.edu>
2024-10-01 12:29:35 -0400

    Fixed bugs

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/dice_roller.dart';
 
 class GradientContainer extends StatelessWidget{
-  GradientContainer(this.startAligment, this.endAlignment, {super.key})
+  GradientContainer(this.startAligment, this.endAlignment, {super.key});
   Alignment startAligment;
   Alignment endAlignment;
 
@@ -16,13 +16,13 @@ class GradientContainer extends StatelessWidget{
           begin: startAligment,
           end: endAlignment,
           colors: const [
-            Color.fromARGB(255, 44, 8, 128)
+            Color.fromARGB(255, 44, 8, 128),
             Color.fromARGB(255, 9, 1, 24)
           ]),
       ),
       child: const Center(
         child: DiceRoller(),
-      ),
+      ), 
     );
   }
 }
